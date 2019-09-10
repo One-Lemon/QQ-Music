8ca82703a9beb609755e2ada612ec3115d635115
one-lemon <qq982516861@gmail.com>
2019-09-11 07:30:46 +0800

    navlink

--- src/pages/music/index/index.js
+++ src/pages/music/index/index.js
@@ -1,11 +1,38 @@
 import React, { Component } from "react";
 import { MIndexWarp } from '../style';
+import PlayList from '../../../components/playList';
 
 export default class MusicIndex extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mainli: [
+        { id: 0, value: '为你推荐' },
+        { id: 1, value: '钢琴' },
+        { id: 2, value: '古筝' },
+        { id: 3, value: '英语' },
+        { id: 4, value: '情歌' },
+        { id: 5, value: '背景音乐' }],
+      isTab: 0
+    }
+  }
   render() {
+    const { mainli, isTab } = this.state;
     return (
       <MIndexWarp>
-        <h1>歌单推荐</h1>
+        <div className="marginBox">
+          <h2 className="title">歌 单 推 荐</h2>
+          <ul className="mainUl">
+            {
+              mainli.map(li => (
+                <li className={isTab === li.id ? 'active' : ''} key={li.id}>{li.value}</li>
+              ))
+            }
+          </ul>
+          <div className="cardList">
+            <PlayList />
+          </div>
+        </div>
       </MIndexWarp>
     );
   }
--- src/pages/music/style.js
+++ src/pages/music/style.js
@@ -1,13 +1,15 @@
 import styled from "styled-components";
 
 export const MusicWarp = styled.div`
-  max-width: 1200px;
   margin: 0 auto;
   .nav {
     display: flex;
-    height: 51px;
+    /* height: 51px; */
     align-items: center;
     padding-left: 230px;
+    max-width: 1200px;
+    width: 1200px;
+    margin: 0 auto;
     li{
       font-size: 15px;
       color: #c1c1c1;
@@ -21,7 +23,38 @@ export const MusicWarp = styled.div`
  * 音乐馆首页歌单推荐央视
  */
 export const MIndexWarp = styled.div`
-  background: url(https://y.gtimg.cn/mediastyle/yqq/img/bg_detail.jpg)center repeat-x;
+  background: url(https://y.gtimg.cn/mediastyle/yqq/img/bg_detail.jpg)50% 0 repeat-x;
   margin: 0 auto;
   height: 480px;
+  .marginBox{  
+    margin: 0 auto;
+    max-width: 1200px;
+    .title{
+    position: relative;
+    padding-top: 4.16667%;
+    padding-bottom: 2%;
+    font-size: 26px;
+    font-weight: bold;
+    width: 196px;
+    margin: 0 auto;
+    box-sizing: border-box;
+    height: 114px;
+    text-align: center;
+  }
+  .mainUl{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    li{
+      margin: 0 24px;
+      cursor: pointer;
+      :hover{
+        color: #31c27c;
+      }
+    }
+    .active{
+      color: #31c27c;
+    }
+  }
+}
 `
